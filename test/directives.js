7c2497005c952223c2a394c7376ea3fedd4d33fd
ulnda <denis.nagibin@gmail.com>
2015-09-17 11:49:31 +0300

    Directive test refactoring

--- test/directives.js
+++ test/directives.js
@@ -1,10 +1,11 @@
 describe('editInPlace', function() {
   beforeEach(module('app'));
 
+  var startValue = 'SomeValue';
   var element, scope;
 
   beforeEach(inject(function(_$compile_, _$rootScope_, _Point_) {
-    _$rootScope_.p = _Point_.build('SomeName', {});
+    _$rootScope_.p = _Point_.build(startValue, {});
     var html = '<edit-in-place data-value="p"></edit-in-place>';
     element = _$compile_(html)(_$rootScope_);
     _$rootScope_.$digest();
@@ -12,14 +13,13 @@ describe('editInPlace', function() {
   }));
 
   it('replace the element with the appropriate content', function() {
-    var html = '<span ng-dblclick="activate()" ng-bind="value.label" ' +
-      'class="ng-binding">SomeName</span>';
+    var html = startValue;
     expect(element.html()).toContain(html);
   });
 
   it('$scope.activate', function() {
     scope.activate();
-    expect(scope.newValue).toEqual('SomeName');
+    expect(scope.newValue).toEqual(startValue);
     expect(element.hasClass('active')).toEqual(true);
   });
 
@@ -30,9 +30,9 @@ describe('editInPlace', function() {
   });
 
   it('$scope.save', function() {
-    var newName = 'SomeNewName';
-    scope.newValue = newName;
+    var newValue = 'SomeNewName';
+    scope.newValue = newValue;
     scope.save();
-    expect(scope.value.label).toEqual(newName);
+    expect(scope.value.label).toEqual(newValue);
   });
 });
